47ee37a89b8d9a95a18a6812e4ebe2035e62269b
Dan H <spag1024@hotmail.com>
2019-06-21 06:13:46 -0500

    set port variable for app.listen

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -17,6 +18,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(3000, () => {
-  debug(chalk.green('listening on 3000'));
+app.listen(port, () => {
+  debug(chalk.green(`listening on ${port}`));
 });
