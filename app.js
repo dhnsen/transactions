8df670917e32e4a7916bc0cc3eba38f86bf6a5da
Dan H <spag1024@hotmail.com>
2019-06-21 05:33:26 -0500

    configured express to use statis files so i can test some things

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
+app.use(morgan('tiny'));
+
 app.get('/', function(req, res){
-    res.send('hello from express');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(3000, function(){
