fd1174e6ac45a03ee9c64a45aca53299b8e1bc46
Dan H <spag1024@hotmail.com>
2019-06-22 16:51:17 -0500

    added a couple transaction mocks, fed them into the index view and listed them on the page. Tried to work with formatting but got a little messed up with bootstrap styles and need to re-think it a bit

--- app.js
+++ app.js
@@ -8,6 +8,62 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const bankTransactions = [{
+  account_id: 'vokyE5Rn6vHKqDLRXEn5fne7LwbKPLIXGK98d',
+  amount: 2307.21,
+  iso_currency_code: 'USD',
+  unofficial_currency_code: null,
+  category: [
+    'Shops',
+    'Computers and Electronics',
+  ],
+  category_id: '19013000',
+  date: '2017-01-29',
+  location: {
+    address: '300 Post St',
+    city: 'San Francisco',
+    region: 'CA',
+    postal_code: '94108',
+    country: 'US',
+    lat: null,
+    lon: null,
+  },
+  name: 'Apple Store',
+  payment_meta: Object,
+  pending: false,
+  pending_transaction_id: null,
+  account_owner: null,
+  transaction_id: 'lPNjeW1nR6CDn5okmGQ6hEpMo4lLNoSrzqDje',
+  transaction_type: 'place',
+}, {
+  account_id: 'XA96y1wW3xS7wKyEdbRzFkpZov6x1ohxMXwep',
+  amount: 78.5,
+  iso_currency_code: 'USD',
+  unofficial_currency_code: null,
+  category: [
+    'Food and Drink',
+    'Restaurants',
+  ],
+  category_id: '13005000',
+  date: '2017-01-29',
+  location: {
+    address: '262 W 15th St',
+    city: 'New York',
+    region: 'NY',
+    postal_code: '10011',
+    country: 'US',
+    lat: 40.740352,
+    lon: -74.001761,
+  },
+  name: 'Golden Crepes',
+  payment_meta: Object,
+  pending: false,
+  pending_transaction_id: null,
+  account_owner: null,
+  transaction_id: '4WPD9vV5A1cogJwyQ5kVFB3vPEmpXPS3qvjXQ',
+  transaction_type: 'place',
+}];
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -17,7 +73,10 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index',
+    {
+      bankTransactions,
+    });
 });
 
 app.listen(port, () => {
